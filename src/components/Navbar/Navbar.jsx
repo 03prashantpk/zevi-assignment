207f60662e8337a272c9d4912e3dfcddb5634343
Prashant Kumar <43730425+03prashantpk@users.noreply.github.com>
2023-12-03 20:21:58 +0530

    vercel config

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,7 +2,7 @@ import SearchBar from "../SearchBar/SearchBar";
 import "./Navbar.scss";
 import Logo from '/logo.png';
 import React from 'react';
-import { useLocation } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 const Navbar = () => {
     const location = useLocation();
@@ -12,6 +12,7 @@ const Navbar = () => {
     return (
         <nav className="Navbar">
             {isProductPage && <SearchBar />}
+            <Link to="/products"><h1>Product</h1></Link>
             <img src={Logo} alt="logo" />
         </nav>
     );
